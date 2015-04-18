671f6696d8ce62c100f152f262bfc8161d9cf398
kcneato <krstec@gmail.com>
2015-04-18 23:28:38 +0200

    test calibration rotation

--- neato_driver/src/neato_driver/neato_driver.py
+++ neato_driver/src/neato_driver/neato_driver.py
@@ -55,8 +55,8 @@ class Botvac():
                       "RFRONTBIT": 0,"BTN_SOFT_KEY": 0,"BTN_SCROLL_UP": 0,"BTN_START": 0,"BTN_BACK": 0,"BTN_SCROLL_DOWN": 0}
         self.stop_state = True
         
-        self.base_width = 248    # millimeters
-        self.max_speed = 300     # millimeters/second
+        self.base_width = 350    # millimeters
+        self.max_speed = 200     # millimeters/second
         self.crtl_z = chr(26)
         # turn things on
         self.port.flushInput()
